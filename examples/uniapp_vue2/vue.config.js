6ebf7debe24830272b494602de6aea34ae3ac040
MellowCo <799478052@qq.com>
2022-07-24 20:03:18 +0800

    chore: add examples

---
+++ examples/uniapp_vue2/vue.config.js
@@ -0,0 +1,11 @@
+const UnoCSS = require('unocss-webpack-uniapp2').default
+const transformWeClass = require('unplugin-transform-we-class/webpack')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      UnoCSS(),
+      transformWeClass(),
+    ],
+  }
+}
